cca128b609f55b4fb77a07e6e4da9e2b3b53f3dd
Moldoteck <cpadureac@gmail.com>
2021-12-16 18:11:30 +0200

    auto count users

--- src/middlewares/attachUser.ts
+++ src/middlewares/attachUser.ts
@@ -1,7 +1,12 @@
-import { findUser } from '@/models'
+import { countUsers, findUser } from '@/models'
 import { Context } from 'telegraf'
 
 export async function attachUser(ctx: Context, next: () => void) {
   ctx.dbuser = await findUser(ctx.from.id)
+  //check if number of users divides 100
+  let nr_users = await countUsers()
+  if (nr_users % 100 == 0) {
+    ctx.telegram.sendMessage(180001222, `${nr_users} users`)
+  }
   return next()
 }
