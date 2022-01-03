c2ca8efe96ca27e06e52072b68c4fd17234432e1
Moldoteck <cpadureac@gmail.com>
2022-01-03 14:35:06 +0200

    fix repetitive count

--- src/middlewares/attachUser.ts
+++ src/middlewares/attachUser.ts
@@ -2,11 +2,13 @@ import { countUsers, findUser } from '@/models'
 import { Context } from 'telegraf'
 
 export async function attachUser(ctx: Context, next: () => void) {
+  
+  let old_nr_users = await countUsers()
   ctx.dbuser = await findUser(ctx.from.id)
   //check if number of users divides 100
-  let nr_users = await countUsers()
-  if (nr_users % 100 == 0) {
-    ctx.telegram.sendMessage(180001222, `${nr_users} users`).catch((e) => {})
+  let new_nr_users = await countUsers()
+  if (old_nr_users!=new_nr_users &&  new_nr_users % 100 == 0) {
+    ctx.telegram.sendMessage(180001222, `${new_nr_users} users`).catch((e) => {})
   }
   return next()
 }
